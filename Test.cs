cd409a8268a7af28e21b71b802b83941e7709eff
PiotrTe <piotrekteter@gmail.com>
2023-04-24 23:02:32 +0100

    Program is functional

--- Test.cs
+++ Test.cs
@@ -1,76 +1,138 @@
 class Test : IValidation
 {
+    static float answer = 0;
+    static string taskString = "";
+
+    static int num1 = 0;
+    static int num2 = 0;
+    static int num3 = 0;
+    static int op1 = 0;
+    static int op2 = 0;
     public static void TestClass()
     {
-        float answer = 0;
-        string taskString = "";
-        int op1 = 0;
-        int op2 = 0;
+        float userInput = 0;
+
+        while (true)
+        {
+            Reset();
+            Menu();
+            if (Pack.hand.Count == 0)
+            {
+                continue;
+            }
+            
+            foreach (Card card in Pack.hand)
+            {
+                if (Pack.hand.IndexOf(card) % 2 == 1)
+                {
+                    if ((int)card.Suit == 0)
+                    {
+                        taskString += " - ";
+                    }
+                    if ((int)card.Suit == 1)
+                    {
+                        taskString += " + ";
+                    }
+                    if ((int)card.Suit == 2)
+                    {
+                        taskString += " * ";
+                    }
+                    if ((int)card.Suit == 3)
+                    {
+                        taskString += " / ";
+                    } 
+                }
+                else
+                {
+                    taskString += (int)card.Rank;
+                }
+            }
+            // clear the console window
+            Console.Clear();
+            System.Console.WriteLine("The answer is: " + answer);
+            System.Console.WriteLine("Your task is: " + taskString);
+
+            userInput = IValidation.GetFloatInput("Please enter your answer: ", -10000, 10000);
+
+            if (userInput == answer)
+            {
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect");
+            }
+        }
+
+
+    }
+    private static void Menu()
+    {
         Pack pack = new Pack();
-        
+        float tempResult = 0;
+
         Console.WriteLine("Welcome to the Maths Tutor Application. \nPlease choose one of the following menu options: \n [1] Instructions \n [2] Play ( 3 Cards ) \n [3] Play ( 5 Cards )\n [4] Exit \n");
         string? input = Console.ReadLine();
         switch (input)
         {
             case "1": // Instructions
-                Console.WriteLine("Instructions: You will be given 3 or 5 cards, cards will represent numbers and operators. \nYou will be asked to calculate the result of the cards. \nYour time and cards will be recorded. \n");
+                Console.WriteLine("\nInstructions: You will be given 3 or 5 cards.\ncards will be represented by numbers and operators.\nYou will be asked to calculate the result of the cards\nand provide it with accuracy of 2 decimal places.\nYour time, amount of tries and current hand will be saved after you win.\n");
                 break;
 
             case "2": // 3 cards
                 Pack.deal(3);
-                answer = pack.calculate((float)Pack.hand[0].Rank, (float)Pack.hand[2].Rank, (int)Pack.hand[1].Suit);
+                num1 = (int)Pack.hand[0].Rank;
+                num2 = (int)Pack.hand[2].Rank;
+                op1 = (int)Pack.hand[1].Suit;
+
+                answer = pack.calculate(num1, num2, op1);
+                answer = (float)Math.Round(answer, 2);
                 break;
 
             case "3": // 5 cards
                 Pack.deal(5);
+                num1 = (int)Pack.hand[0].Rank;
+                num2 = (int)Pack.hand[2].Rank;
+                num3 = (int)Pack.hand[4].Rank;
                 op1 = (int)Pack.hand[1].Suit;
                 op2 = (int)Pack.hand[3].Suit;
-                // swap the order of the operators depending on the order of the operators according to the BODMAS rule
+
                 if (op1 < op2)
                 {
-                    Card temp = Pack.hand[1];
-                    Pack.hand[1] = Pack.hand[3];
-                    Pack.hand[3] = temp;
+                    tempResult = pack.calculate(num2 , num3, op2);
+                    answer = pack.calculate(num1, tempResult, op1);
+                    answer = (float)Math.Round(answer, 2);
                 }
-
-                answer = pack.calculate(pack.calculate((float)Pack.hand[0].Rank, (float)Pack.hand[2].Rank, (int)Pack.hand[1].Suit), (float)Pack.hand[4].Rank, (int)Pack.hand[3].Suit);
+                else
+                {
+                    tempResult = pack.calculate(num1, num2, op1);
+                    answer = pack.calculate(tempResult, num3, op2);
+                    answer = (float)Math.Round(answer, 2);
+                }
                 break;
 
             case "4": // Exit
                 Console.WriteLine("Thank you for playing");
+                // wait 2 seconds
+                Thread.Sleep(2000);
+                // exit the program
+                Environment.Exit(0);
                 break;
 
             default:  // Invalid input
                 Console.WriteLine("Invalid input");
                 break;
         }
-        System.Console.WriteLine("The answer is: " + answer);
-        foreach (Card card in Pack.hand)
-        {
-            if (Pack.hand.IndexOf(card) % 2 == 1)
-            {
-                if ((int)card.Suit == 0)
-                {
-                    taskString += " - ";
-                }
-                if ((int)card.Suit == 1)
-                {
-                    taskString += " + ";
-                }
-                if ((int)card.Suit == 2)
-                {
-                    taskString += " * ";
-                }
-                if ((int)card.Suit == 3)
-                {
-                    taskString += " / ";
-                } 
-            }
-            else
-            {
-                taskString += (int)card.Rank;
-            }
-        }
-        System.Console.WriteLine("Your task is: " + taskString);
+    }
+    private static void Reset()
+    {
+        answer = 0;
+        taskString = "";
+        num1 = 0;
+        num2 = 0;
+        num3 = 0;
+        op1 = 0;
+        op2 = 0;
+        Pack.hand.Clear();
     }
 }
--- Validation.cs
+++ Validation.cs
@@ -66,4 +66,70 @@
             }
             return intInput; // Return the valid input value
         }
+        // A method for validating if an input is a float or not
+        private static float ValidateFloat(string input)                                         
+        {
+            float number;
+            bool isValid = float.TryParse(input, out number); // Try to parse the input as a float
+            if (isValid == false)
+            {
+                Console.WriteLine($"Value of '{input}' is not float"); // If the input is not a float, display an error message
+                return -3.402823E+38f;
+            }
+            else
+            {
+                return number; // If the input is a float, return its value
+            }
+        }
+
+        // A method for validating if a float is within a certain range or not
+        private static bool ValidateFloatRange(float input, float min, float max)                
+        {
+            if (input < min || input > max) // If the input is outside the given range, display an error message
+            {
+                Console.WriteLine($"Value of {input} is outside of acceptable boundaries ({min}-{max})");
+                return false;
+            }
+            else
+            {
+                return true; // If the input is within the given range, return true
+            }
+        }
+
+        // A method for getting a float input from the user within a certain range
+
+        protected static float GetFloatInput(string inputMessage, float min, float max)          
+        {
+            string? input;
+            float floatInput;
+            while (true) // Keep asking for input until valid input is given
+            {
+                Console.WriteLine($"{inputMessage}"); // Display the input message
+                input = Console.ReadLine(); // Get input from the user
+                if (input == null)
+                {
+                    Console.WriteLine("Input cannot be empty");
+                    continue;
+                }
+                floatInput = ValidateFloat(input); // Validate the input as a float
+
+                if (floatInput == -3.402823E+38f) // If the input is not valid, ask for input again
+                {
+                    continue;
+                }
+                else
+                {
+                    // If the input is within the given range, return the input value
+                    if (ValidateFloatRange(floatInput, min, max))                               
+                    {
+                        break;
+                    }
+                    else // If the input is not within the given range, ask for input again
+                    {
+                        continue;
+                    }
+                }
+            }
+            return floatInput; // Return the valid input value
+        }
     }
